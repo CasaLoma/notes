5ab76c0e8e4e8488a2d2055035e70f1d486ff03b
Brandon <brandonsitu1@gmail.com>
2024-11-23 21:08:15 -0500

    Update lab12.sql

--- lab12.sql
+++ lab12.sql
@@ -69,3 +69,49 @@ SELECT
 FROM courses 
 WHERE 
   lab_day = 'Friday';
+
+Exercise 3
+  -- Upcoming Assignments
+Select *
+from assignments
+where due_date > '2024-11-23'
+
+Exercise 4
+-- Count Assignments by Status
+SELECT 
+  status, 
+  COUNT(*) AS "Number of Assignments" 
+FROM assignments 
+GROUP BY 
+  status;
+
+Exercise 5
+--Longest Course Name
+SELECT 
+  course_name 
+FROM courses 
+ORDER BY 
+  LENGTH(course_name) DESC
+LIMIT 1;
+
+Exercise 6
+-- Uppercase Course Names
+SELECT 
+  UPPER(course_name) AS "Uppercase Course Names" 
+FROM courses;
+
+Exercise 7 
+-- Assignments Due in September
+SELECT 
+  title 
+FROM assignments 
+WHERE 
+  due_date LIKE '%-09-%';
+
+Exercise 8 
+-- Assignments with Missing Due Dates
+SELECT 
+  * 
+FROM assignments 
+WHERE 
+  due_date IS NULL;
